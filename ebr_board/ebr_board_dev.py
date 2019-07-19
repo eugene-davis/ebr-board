17e8fbd7ac2cb4e0a29ab7eb61fb19fa32c9aa4a
Eugene Davis <eugene.davis@tomtom.com>
2019-07-19 15:20:24 +0200

    Handle pylint

--- ebr_board/ebr_board_dev.py
+++ ebr_board/ebr_board_dev.py
@@ -1,10 +1,10 @@
 """
 Used for launching the dev-mode of the erb_board. **Don't use in production**.
 """
-import ebr_board
+from ebr_board import create_app  # pylint: disable=no-name-in-module
 
 if __name__ == "__main__":
-    ebr_board.create_app(
+    create_app(
         config_filename="config.yaml",
         vault_config_filename="vault.yaml",
         vault_creds_filename="vault.yaml",
