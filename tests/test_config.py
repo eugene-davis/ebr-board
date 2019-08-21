9d5db98f719d0135ebed1f7649b1f2fe586613b5
Eugene Davis <eugene.davis@tomtom.com>
2019-08-21 17:23:49 +0200

    Update test_config to safely test vault_anyconfig instatiation

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,9 +5,10 @@ from copy import deepcopy
 from ebr_board.config import VaultConfig
 
 
+@patch("ebr_board.config.VaultAnyConfig.isfile")
 @patch("ebr_board.config.connections.create_connection")
 @patch("ebr_board.config.VaultAnyConfig")
-def test_config(mock_va, mock_connections):
+def test_config(mock_va, mock_connections, mock_isfile):
     """
     Tests that the instantiation of a config.VaultConfig object results in the correct configuration.
     """
@@ -15,14 +16,18 @@ def test_config(mock_va, mock_connections):
 
     local_sample_config = deepcopy(sample_config)
     mock_va.return_value.auto_auth = Mock(return_value=True)
-    mock_va.return_value.load = Mock(return_value=local_sample_config)
+    mock_va.return_value.loads = Mock(return_value=local_sample_config)
 
-    config = VaultConfig("config.yaml", "vault.yaml", "vault_creds.yaml")
+    config = VaultConfig(
+        "test_not_a_conf_file.blargh", "test_not_a_vault_conf_file.blargh", "test_not_a_vault_creds_file.blargh"
+    )
 
     # Validate calls to VaultAnyConfig instance
-    mock_va.assert_called_once_with("vault.yaml", ac_parser=None)
-    mock_va.return_value.auto_auth.assert_called_once_with("vault_creds.yaml", ac_parser=None)
-    mock_va.return_value.load.assert_called_once_with("config.yaml", process_secret_files=False)
+    mock_va.assert_called_once_with("test_not_a_vault_conf_file.blargh", ac_parser=None)
+    mock_va.return_value.auto_auth.assert_called_once_with("test_not_a_vault_creds_file.blargh", ac_parser=None)
+    mock_va.return_value.loads.assert_called_once_with(
+        "test_not_a_conf_file.blargh", ac_parser=None, process_secret_files=False
+    )
 
     # Validate elastic configuration
     elastic_config = deepcopy(sample_config["elastic"])
